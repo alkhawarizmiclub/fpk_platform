03695365b9b04f354204bc3e33d46d382448dd5f
itsachrafmansari <contact@achrafmansari.com>
2024-04-30 23:20:08 +0100

    Created the Staff component

---
+++ frontend/src/components/Staff.jsx
@@ -0,0 +1,22 @@
+const Staff = ({ first_name, last_name, occupation, img_src }) => {
+
+    let bg_img = "";
+
+    if (img_src) {
+        bg_img = `bg-[url(${img_src})]`;
+    } else {
+        bg_img = "bg-slate-300";
+    }
+
+    return (
+        <div className="flex flex-col items-center text-center">
+            <div className={`mb-5 max-w-48 w-full aspect-square rounded-full overflow-hidden bg-slate-300`}>
+                <img src={img_src} alt={`${first_name} ${last_name}'s portrait image`} className="mb-5 max-w-48 w-full rounded-full bg-gray-200" />
+            </div>
+            <h1 className="text-slate-800 text-lg font-semibold">{first_name} {last_name}</h1>
+            <h2 className="text-slate-500">{occupation}</h2>
+        </div>
+    );
+}
+
+export default Staff;
