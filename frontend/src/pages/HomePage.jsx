86ed297556463c2980916c3ed343aac9eee9424b
itsachrafmansari <contact@achrafmansari.com>
2024-04-30 23:24:54 +0100

    Made many modifications on the Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import HomeGateElement from '../components/HomeGateElement';
 import SectionTitle from '../components/SectionTitle';
 import Activity from '../components/Activity';
 import Major from '../components/Major';
+import Staff from '../components/Staff';
+import MajorCategory from '../components/MajorCategory';
 
 const HomePage = () => {
     return (
@@ -27,14 +29,14 @@ const HomePage = () => {
                         <p>De par sa vocation la Faculté Polydisciplinaire de Khouribga offre des formations diversifiées couvrant plusieurs champs disciplinaires sous forme de filières à enseignement modulaire.</p>
                     </div>
                     <div>
-                        <iframe
+                        {/* <iframe
                             className="w-full aspect-video rounded-lg border border-slate-300"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1140.7043195271508!2d-6.905736303784177!3d32.869769894863275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda427000bc5899f%3A0xba7e39743b7a569b!2sFacult%C3%A9%20Polydisciplinaire%20de%20Khouribga!5e0!3m2!1sen!2sma!4v1714438474595!5m2!1sen!2sma"
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
-                        ></iframe>
-                        {/* <div className="aspect-video rounded-lg bg-cover bg-norepeat bg-center bg-[url(/map.jpg)]"></div> */}
+                        ></iframe> */}
+                        <div className="aspect-video rounded-lg bg-cover bg-norepeat bg-center bg-[url(/map.jpg)]"></div>
                     </div>
                 </div>
             </section>
@@ -51,22 +53,31 @@ const HomePage = () => {
 
             <section className="p-10">
                 <SectionTitle title="staff administratif" />
-                To Be Filled
+                <div className="p-10 grid grid-cols-4 gap-5">
+                    <Staff first_name="Khalid" last_name="Mehdi" occupation="Occupation" img_src="https://i0.wp.com/www.fpk.ac.ma/wp-content/uploads/2023/02/doyen-1-scaled-e1677513352365.jpg" />
+                    <Staff first_name="Khalid" last_name="Sbiaai" occupation="Vice Doyen chargé à la pédagogie" img_src="http://www.fpk.ac.ma/wp-content/uploads/2022/12/vdoyen-e1670942309390.jpeg" />
+                    <Staff first_name="Noureddine" last_name="Barka" occupation="Vice Doyen chargé de la recherche scientifique et de la coopération" img_src="https://i0.wp.com/www.fpk.ac.ma/wp-content/uploads/2024/03/Noureddine-Barka.jpg" />
+                    <Staff first_name="Omar" last_name="Hansali" occupation="Secrétaire Générale" img_src="https://i0.wp.com/www.fpk.ac.ma/wp-content/uploads/2023/03/IMG-20230310-WA0003-e1678463961724.jpg" />
+                </div>
             </section>
 
             <section className="p-10">
                 <SectionTitle title="filières" />
                 <div className="p-4 flex justify-center gap-5">
-                    <div className="border-b border-orange-400">Licence</div>
-                    <div className="border-b border-orange-400">Master</div>
-                    <div className="border-b border-orange-400">Doctorat</div>
+                    <MajorCategory label="licences fondamentales"/>
+                    <MajorCategory label="licences professionnelles"/>
+                    <MajorCategory label="master"/>
+                    <MajorCategory label="doctorat"/>
                 </div>
                 <div className="grid grid-cols-5 gap-5">
                     <Major label="science mathématique et informatique" url="#" img_src="https://fpk-biblio.netlify.app/img/smi.jpg" />
                     <Major label="science mathématique et application" url="#" img_src="https://fpk-biblio.netlify.app/img/sma.jpg" />
                     <Major label="science economie et gestion" url="#" img_src="https://fpk-biblio.netlify.app/img/seg.jpg" />
-                    <Major label="GEO" url="#" img_src="https://fpk-biblio.netlify.app/img/geo.jpg" />
-                    <Major label="LA" url="#" img_src="https://fpk-biblio.netlify.app/img/ea.jpg" />
+                    <Major label="science de la matière physique" url="#" img_src="https://fpk-biblio.netlify.app/img/smp.jpg" />
+                    <Major label="science de la matière chimie" url="#" img_src="https://fpk-biblio.netlify.app/img/smc.jpg" />
+                    <Major label="sciences de la Vie" url="#" img_src="https://fpk-biblio.netlify.app/img/sv.jpg" />
+                    <Major label="géographie" url="#" img_src="https://fpk-biblio.netlify.app/img/geo.jpg" />
+                    <Major label="études arabe" url="#" img_src="https://fpk-biblio.netlify.app/img/ea.jpg" />
                 </div>
             </section>
 
@@ -76,7 +87,7 @@ const HomePage = () => {
             </section>
 
             <section className="p-10">
-                <SectionTitle title="activites & événements" />
+                <SectionTitle title="activités & événements" />
                 <Activity />
                 <Activity />
                 <Activity />
