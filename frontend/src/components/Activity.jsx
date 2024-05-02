9528e326a178e1816b25d8536c6753f249313359
itsachrafmansari <contact@achrafmansari.com>
2024-05-02 09:34:46 +0100

    Updated the background of the Activity compnoent

--- frontend/src/components/Activity.jsx
+++ frontend/src/components/Activity.jsx
@@ -6,7 +6,7 @@ const Activity = ({ label, url, img_src }) => {
                 <span className="text-3xl font-semibold">23</span>
                 <span>2024</span>
             </div>
-            <a href={url} className="absolute bottom-0 p-5 flex flex-col justify-center w-full h-20 group-hover:h-full transition-all duration-300 text-white bg-orange-400/50 backdrop-blur">
+            <a href={url} className="absolute bottom-0 p-5 flex flex-col justify-center w-full h-20 group-hover:h-full transition-all duration-300 text-white bg-gradient-to-t from-orange-400 via-orange-300 via-80% group-hover:via-100% backdrop-blur">
                 <span className="text-lg truncate group-hover:whitespace-normal">{label}</span>
             </a>
             <img src={img_src} alt={label} className="object-cover h-full w-full" />
