aece5f23ca432eec189645adb82a0fb5137c63ad
itsachrafmansari <contact@achrafmansari.com>
2024-05-03 19:13:24 +0100

    Modified the layout of the Major component

--- frontend/src/components/Major.jsx
+++ frontend/src/components/Major.jsx
@@ -1,11 +1,9 @@
 const Major = ({ label, url, img_src }) => {
 
     return (
-        <div className={`relative group overflow-hidden aspect-video bg-cover bg-norepeat bg-center`}>
-            <a href={url} className=" p-5 absolute transition-all duration-300 ease-in-out z-0 h-full w-full flex justify-center items-center text-center capitalize text-white top-1 group-hover:top-0 -translate-y-full group-hover:translate-y-0 bg-orange-400/100 group-hover:bg-orange-400/50 backdrop-blur">{label}</a>
-            {img_src && (
-                <img src={img_src} />
-            )}
+        <div className="relative group overflow-hidden aspect-video rounded-lg">
+            <a href={url} className="absolute h-full w-full flex justify-center items-center capitalize text-center text-white text-lg font-bold transition-all duration-300 ease-in-out bg-orange-400/30 group-hover:bg-orange-400/90">{label}</a>
+            <img src={img_src} />
         </div>
     );
 
