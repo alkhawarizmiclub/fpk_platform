b7262d7158f9b13d46a3183bb50896dda57d6ff0
itsachrafmansari <contact@achrafmansari.com>
2024-05-06 18:22:56 +0100

    Modified the layout of the Activity component

--- frontend/src/components/Activity.jsx
+++ frontend/src/components/Activity.jsx
@@ -1,27 +1,36 @@
-const Activity = ({ label, url, img_src, date_day, date_month, date_year }) => {
+import { faCalendar, faMapPin } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Activity = ({ label, url, img_src, date_day, date_month, date_year, location }) => {
     return (
-        // <div className="relative group flex justify-center items-center rounded-lg aspect-[4/3] overflow-hidden bg-slate-200">
-        //     <div className="absolute z-[1] top-0 right-5 group-hover:-right-20 p-2 max-w-32 flex flex-col justify-center items-center rounded-b transition-all duration-300 text-white shadow bg-orange-400">
-        //         <span className="text-lg">Jul</span>
-        //         <span className="text-3xl font-semibold">23</span>
-        //         <span>2024</span>
-        //     </div>
-        //     <a href={url} className="absolute z-0 bottom-0 p-5 flex flex-col justify-center w-full h-20 group-hover:h-full transition-all duration-300 text-white bg-orange-400">
-        //         <span className="text-lg truncate group-hover:whitespace-normal">{label}</span>
-        //     </a>
-        //     <img src={img_src} alt={label} className="object-cover h-full w-full" />
-        // </div>
-        <div className="relative flex flex-col gap-2">
-            <div className="absolute z-[1] top-0 right-5 group-hover:right-5 p-2 max-w-32 flex flex-col justify-center items-center rounded-b transition-all duration-300 text-white shadow bg-orange-400">
-                <span className="text-lg">Jul</span>
-                <span className="text-3xl font-semibold">23</span>
-                <span>2024</span>
+        <div className="relative group h-min overflow-hidden rounded-lg shadow bg-white">
+
+            <div className="absolute z-[1] top-0 right-5 p-2 max-w-32 flex flex-col justify-center items-center rounded-b text-white shadow bg-orange-400/80">
+                <span className="text-lg">{date_month.slice(0, 3)}</span>
+                <span className="text-3xl font-semibold">{date_day}</span>
             </div>
-            <div className="w-full aspect-video flex overflow-hidden border-b-4 border-orange-400 bg-slate-200">
-                <img src={img_src} alt={label} className="w-full h-full object-cover" />
+
+            <div className="w-full aspect-video">
+                <img src={img_src} alt={label} className="object-cover h-full w-full" />
             </div>
-            <a href={url} className="text-lg font-semibold">{label}</a>
-            {/* <div className="text-slate-600">{date_day} {date_month} {date_year}</div> */}
+
+            <div className="p-5 space-y-4">
+
+                <a href={url} className="text-lg font-bold">{label}</a>
+
+                <div className="flex justify-between text-slate-60 text-sm">
+                    <div className="flex justify-center items-center gap-2">
+                        <FontAwesomeIcon icon={faCalendar} className="text-orange-400"/>
+                        <span>{date_day} {date_month} {date_year}</span>
+                    </div>
+                    <div className="flex justify-center items-center gap-2">
+                        <FontAwesomeIcon icon={faMapPin} className="text-orange-400"/>
+                        <span>{location}</span>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
