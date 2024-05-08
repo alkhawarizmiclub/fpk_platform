247899b68c3865e99d8e3755073fc28401950005
itsachrafmansari <contact@achrafmansari.com>
2024-05-08 16:08:06 +0100

    Modified the layout of the NavBar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import NavbarDropMenu from "./NavbarDropMenu";
+import { Paths } from "../routers/router";
+import Button from "./Button";
 
 const Navbar = () => {
 
@@ -7,48 +9,51 @@ const Navbar = () => {
         {
             label: "ETABLISSEMENT",
             children: [
-                {label: "Mot du Doyen", url: "url 1"},
-                {label: "Présentation", url: "url 2"},
-                {label: "Staff administratif", url: "url 3"},
-                {label: "Départements", url: "url 3"},
-                {label: "Réglementation", url: "url 3"},
-                {label: "Galerie", url: "url 3"},
-                {label: "Video", url: "url 3"}
+                { label: "Mot du Doyen", url: "url 1" },
+                { label: "Présentation", url: Paths.PRESENTATION_PAGE },
+                { label: "Staff administratif", url: Paths.STAFF_PAGE },
+                { label: "Départements", url: "/url 3" },
+                { label: "Réglementation", url: "/url 3" },
+                { label: "Galerie", url: "/url 3" },
+                { label: "Video", url: "/url 3" }
             ]
         },
         {
             label: "FORMATION",
             children: [
-                {label: "Licence Fondamentale", url: "/lf"},
-                {label: "Licence Professionnelle", url: "/lp"},
-                {label: "Master", url: "/master"},
-                {label: "Doctorat", url: "/doctorat"}
+                { label: "Licence Fondamentale", url: "/lf" },
+                { label: "Licence Professionnelle", url: "/lp" },
+                { label: "Master", url: "/master" },
+                { label: "Doctorat", url: "/doctorat" }
             ]
         },
         {
             label: "ESPACE ETUDIANTS",
             children: [
-                {label: "Evénements", url: "url 1"},
-                {label: "Clubs", url: "url 1"},
-                {label: "Activités para-universitaires", url: "url 1"}
+                { label: "Evénements", url: "url 1" },
+                { label: "Clubs", url: "url 1" },
+                { label: "Activités para-universitaires", url: "url 1" }
             ]
         },
         {
             label: "RECHERCHE & COOPÉRATION",
             children: [
-                {label: "Laboratoires", url: "url 1"},
-                {label: "Thèses", url: "url 1"}
+                { label: "Laboratoires", url: "url 1" },
+                { label: "Thèses", url: "url 1" }
             ]
         },
         {
-            label: "CONTACT"
+            label: "CONTACT",
+            url: "/contact"
         }
     ]
 
     return (
-        <nav className="z-[1] py-5 px-10 h-24 flex justify-between items-center shadow bg-white">
+        <nav className="z-10 py-5 px-10 h-24 flex justify-between items-center shadow bg-white">
 
-            <img src="/fpk_logo.svg" className="h-full" />
+            <Link to={Paths.HOME_PAGE} className="h-full">
+                <img src="/fpk_logo.svg" className="h-full" />
+            </Link>
 
             <ul className="flex gap-5 font-semibold">
                 {navLinks.map(({ label, children }) => (
@@ -57,8 +62,12 @@ const Navbar = () => {
             </ul>
 
             <div className="flex gap-4">
-                <Link type="button" className="py-3 px-6 rounded-lg text-white font-semibold bg-orange-400" to="/login">Login</Link>
-                <Link type="button" className="py-3 px-6 rounded-lg text-orange-400 font-semibold border border-orange-400" to="/apply">Apply</Link>
+                <Button plain_bg={true}>
+                    <Link className="py-3 px-6 block" to={Paths.LOGIN_PAGE}>Login</Link>
+                </Button>
+                <Button>
+                    <Link className="py-3 px-6 block" to={Paths.APPLY_PAGE}>Apply</Link>
+                </Button>
             </div>
 
         </nav>
