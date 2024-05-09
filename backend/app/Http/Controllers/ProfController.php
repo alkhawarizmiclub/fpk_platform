fc1ed536fab87cc563f25855309336e2584b2716
Hicham Benoukaiss <hibenouk@e1r11p4.1337.ma>
2024-05-09 16:50:15 +0100

    create (student, prof, admin ) basic db shema and add student input validition

---
+++ backend/app/Http/Controllers/ProfController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProfRequest;
+use App\Http\Requests\UpdateProfRequest;
+use App\Models\Prof;
+
+class ProfController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProfRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Prof $prof)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Prof $prof)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProfRequest $request, Prof $prof)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Prof $prof)
+    {
+        //
+    }
+}
