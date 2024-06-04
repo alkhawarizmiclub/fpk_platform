7b4e3c999ea4992fd40c8bca47416afd3893fb68
itsachrafmansari <contact@achrafmansari.com>
2024-06-04 16:01:18 +0100

    Modified the ENT dashboard to not show the dashboard page button

--- frontend/src/pages/ent/student/EntStudentDashbordPage.jsx
+++ frontend/src/pages/ent/student/EntStudentDashbordPage.jsx
@@ -6,12 +6,14 @@ const EntStudentDashbordPage = () => {
 
     return (
         <div className="p-5 grid grid-cols-5 gap-5">
-            {EntStudentPagesUrlsList.map(({ label, url, icon, bg }, i) => (
-                <Link to={url} key={i} className="aspect-square flex flex-col justify-center items-center gap-2 rounded-lg shadow bg-white">
-                    <div><FontAwesomeIcon icon={icon} className={`p-5 text-5xl rounded-lg text-white ${bg}`}/></div>
-                    <div className="capitalize text-xl">{label}</div>
-                </Link>
-            ))}
+            {EntStudentPagesUrlsList.map(({ label, url, icon, bg, isHome }, i) => 
+                !isHome && (
+                    <Link to={url} key={i} className="aspect-square flex flex-col justify-center items-center gap-2 rounded-lg shadow bg-white">
+                        <div><FontAwesomeIcon icon={icon} className={`p-5 text-5xl rounded-lg text-white ${bg}`} /></div>
+                        <div className="capitalize text-xl">{label}</div>
+                    </Link>
+                )
+            )}
         </div>
     );
 }
--- frontend/src/pages/ent/teacher/EntTeacherDashbordPage.jsx
+++ frontend/src/pages/ent/teacher/EntTeacherDashbordPage.jsx
@@ -6,11 +6,13 @@ const EntTeacherDashbordPage = () => {
 
     return (
         <div className="p-5 grid grid-cols-5 gap-5">
-            {EntTeacherPagesUrlsList.map(({ label, url, icon, bg }, i) => (
-                <Link to={url} key={i} className="aspect-square flex flex-col justify-center items-center gap-2 rounded-lg shadow bg-white">
-                    <div><FontAwesomeIcon icon={icon} className={`p-5 text-5xl rounded-lg text-white ${bg}`}/></div>
-                    <div className="capitalize text-xl">{label}</div>
-                </Link>
+            {EntTeacherPagesUrlsList.map(({ label, url, icon, bg, isHome }, i) => (
+                !isHome && (
+                    <Link to={url} key={i} className="aspect-square flex flex-col justify-center items-center gap-2 rounded-lg shadow bg-white">
+                        <div><FontAwesomeIcon icon={icon} className={`p-5 text-5xl rounded-lg text-white ${bg}`} /></div>
+                        <div className="capitalize text-xl">{label}</div>
+                    </Link>
+                )
             ))}
         </div>
     );
