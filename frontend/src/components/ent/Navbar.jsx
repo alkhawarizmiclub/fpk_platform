db5748d72bd9cd21df1cfb8a03ecd8ee26f96039
itsachrafmansari <contact@achrafmansari.com>
2024-06-04 23:36:48 +0100

    Used the UserContext in the ENT Navbar

--- frontend/src/components/ent/Navbar.jsx
+++ frontend/src/components/ent/Navbar.jsx
@@ -1,18 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { useUserContext } from "../../contexts/context";
 
 const Navbar = () => {
 
-    let fullname = "Achraf Mansari";
-    let major = "Science Mathématique et Informatique";
-    let semester = "S6"
+    const context = useUserContext();
+
+    const fullname = `${context.user.firstName} ${context.user.lastName}`;
 
     return (
         <nav className="z-10 p-5 h-20 flex justify-between items-center shadow bg-white">
 
             <div>
                 <div className="font-semibold">{fullname}</div>
-                <div className="text-sm text-slate-500">{major} - {semester}</div>
+                <div className="text-sm text-slate-500">{context.user.currentMajor} - {context.user.currentSemester}</div>
             </div>
 
             <button className="flex items-center">
