03fa2a1275cd930a0f995273a48c7400cc797cd3
itsachrafmansari <contact@achrafmansari.com>
2024-06-04 16:00:20 +0100

    Added EntPagesUrlsList to the ENT's Sidebar and EntLayout component

--- frontend/src/components/ent/Sidebar.jsx
+++ frontend/src/components/ent/Sidebar.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 import Paths from "../../routers/Paths.json";
-import EntStudentPagesUrlsList from "./EntStudentPagesUrlsList";
 import SidebarMenuItem from "./SidebarMenuItem";
-import { faHome, faSignOut } from "@fortawesome/free-solid-svg-icons";
+import { faSignOut } from "@fortawesome/free-solid-svg-icons";
 
-const Sidebar = () => {
+const Sidebar = ({ EntPagesUrlsList }) => {
 
     return (
         <aside className="p-5 w-full max-w-xs flex flex-col justify-between bg-slate-700">
@@ -15,8 +14,7 @@ const Sidebar = () => {
                 </Link>
 
                 <div className="flex flex-col gap-1">
-                    <SidebarMenuItem label="Home" url={Paths.E_STUDENT_DASHBOARD_PAGE} icon={faHome} />
-                    {EntStudentPagesUrlsList.map(({ label, url, icon }, i) => <SidebarMenuItem label={label} url={url} icon={icon} key={i} />)}
+                    {EntPagesUrlsList.map(({ label, url, icon }, i) => <SidebarMenuItem label={label} url={url} icon={icon} key={i} />)}
                 </div>
             </div>
 
