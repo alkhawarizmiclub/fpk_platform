6b549e212e8771717f2c1035ba85f431caf2999c
itsachrafmansari <contact@achrafmansari.com>
2024-06-05 11:16:50 +0100

    Completed the EntStudentAccountsPage

--- frontend/src/pages/ent/student/EntStudentAccountsPage.jsx
+++ frontend/src/pages/ent/student/EntStudentAccountsPage.jsx
@@ -13,49 +13,24 @@ const EntStudentAccountsPage = () => {
     ]
 
     return (
-
         <EntPageContainer title="Comptes">
-            {/* <table className="w-full hidden md:flex rounded shadow overflow-hidden">
-                <tbody>
-                    <tr className="text-white bg-slate-800">
-                        <th className="p-3">Compte</th>
-                        <th className="p-3">Email</th>
-                        <th className="p-3">Mot de passe</th>
-                        <th className="p-3">Lien de connexion</th>
-                    </tr>
-                    {accounts.map(({ label, email, password, loginURL }) =>
-                        <tr className="text-slate-700 odd:bg-gray-100">
-                            <td className="p-3 text-center">{label}</td>
-                            <td className="p-3 text-center">{email}</td>
-                            <td className="p-3 text-center"><EntStudentInscriptionPassword password={password} /></td>
-                            <td className="p-3 text-center underline"><a href={loginURL}><FontAwesomeIcon icon={faUpRightFromSquare} /></a></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table> */}
-
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
                 {accounts.map(({ label, email, password, loginURL }, i) =>
-                    <div key={i} className="p-5 flex items-center rounded shadow">
-                        <div className="grow space-y-3">
-                            <div className="w-full flex justify-between items-center">
-                                <div>
-                                    <div className="text-sm text-slate-700 font-semibold">Platform</div>
-                                    <div className="text-slate-500">{label}</div>
-
-                                </div>
-                                <a href={loginURL} target="_blank" className="text-slate-500"><FontAwesomeIcon icon={faUpRightFromSquare} /></a>
-                            </div>
+                    <div key={i} className="p-5 grow space-y-3 rounded shadow">
+                        <div className="w-full flex justify-between items-center">
                             <div>
-                                <div className="text-sm text-slate-700 font-semibold">Email</div>
-                                <div className="text-slate-500">{email}</div>
+                                <div className="text-sm text-slate-700 font-semibold">Platform</div>
+                                <div className="text-slate-500">{label}</div>
                             </div>
-                            <div>
-                                <div className="text-sm text-slate-700 font-semibold">Password</div>
-                                <div className="text-slate-500"><EntStudentInscriptionPassword password={password} /></div>
-                            </div>
+                            <a href={loginURL} target="_blank" className="text-slate-500"><FontAwesomeIcon icon={faUpRightFromSquare} /></a>
                         </div>
                         <div>
+                            <div className="text-sm text-slate-700 font-semibold">Email</div>
+                            <div className="text-slate-500">{email}</div>
+                        </div>
+                        <div>
+                            <div className="text-sm text-slate-700 font-semibold">Password</div>
+                            <div className="text-slate-500"><EntStudentInscriptionPassword password={password} /></div>
                         </div>
                     </div>
                 )}
