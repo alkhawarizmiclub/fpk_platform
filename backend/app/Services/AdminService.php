766def18be9d787862c69c52c24b0126a191a492
Hicham Benoukaiss <hibenouk@e1r11p4.1337.ma>
2024-06-06 16:53:21 +0100

    add status to some unvalid  request

--- backend/app/Services/AdminService.php
+++ backend/app/Services/AdminService.php
@@ -1,23 +1,48 @@
 <?php
 
 use App\Traits\JsonTemplate;
+use App\Models\Admin;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\AdminAuth\LoginRequest;
 use App\Models\Student;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AdminService
 {
     use JsonTemplate;
-    // public function generateNote()
-    // {
-    //     Student::chunk(100, function ($students) {
-    //         foreach ($students as $student) {
-    //             $note = 0;
-    //             foreach ($student->results as $result) {
-    //                 $note += $result->note;
-    //             }
-    //             $note = $note / count($student->results);
-    //             $student->note = $note;
-    //             $student->save();
-    //         }
-    //     });
-    // }
+
+    public function generateReleveNote()
+    {
+        Student::chunk(100, function ($students) {
+            foreach ($students as $student) {
+
+            }
+        });
+    }
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+        $admin = Admin::where('email', $request->email)->first();
+        $admin->tokens()->delete();
+        $token = $admin->createToken('api_token', ['role:admin'], Carbon::now()->addHours((int)env('A_TOKEN_EXPIRATION', 2)));
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'logged in successfully',
+                'admin' => $admin,
+                'token' => $token->plainTextToken,
+            ]
+        );
+    }
+    public function logout(Request $request): JsonResponse
+    {
+        Auth::guard('admin')->logout();
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'logged out successfully'
+        ]);
+    }
 }
