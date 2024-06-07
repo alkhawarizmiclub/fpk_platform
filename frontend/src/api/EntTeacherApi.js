d81c8ff887042baac21024f5dc0e59d6c4e21cfd
itsachrafmansari <contact@achrafmansari.com>
2024-06-07 12:32:16 +0100

    Created the EntTeacherApi and EntStaffApi

---
+++ frontend/src/api/EntTeacherApi.js
@@ -0,0 +1,13 @@
+const EntTeacherApi = {
+    login: (email, password) => {},
+    getAccountsData: () => {
+        return [
+            { label: "Microsoft Office", email: "firstname.lastname@usms.ac.ma", password: "thisIsYourPassword", loginURL: "http://www.google.com" },
+            { label: "Microsoft Office", email: "firstname.lastname@usms.ac.ma", password: "thisIsYourPassword", loginURL: "http://www.google.com" },
+            { label: "Microsoft Office", email: "firstname.lastname@usms.ac.ma", password: "thisIsYourPassword", loginURL: "http://www.google.com" },
+            { label: "Microsoft Office", email: "firstname.lastname@usms.ac.ma", password: "thisIsYourPassword", loginURL: "http://www.google.com" }
+        ]
+    }
+}
+
+export default EntTeacherApi;
