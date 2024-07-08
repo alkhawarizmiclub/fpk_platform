66d2f0d266a255d989ad045fad28c1e5d1674ab0
itsachrafmansari <contact@achrafmansari.com>
2024-07-08 15:52:51 +0100

    Updated the CRUD functionalities of the Student Complaints Page

--- frontend/src/pages/ent/student/Complaints/EntStudentComplaintsPage.jsx
+++ frontend/src/pages/ent/student/Complaints/EntStudentComplaintsPage.jsx
@@ -1,45 +1,57 @@
-import { faPlus, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faSpinner, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import EntStudentApi from "../../../../api/EntStudentApi";
 import EntPageContainer from "../../../../components/ent/EntPageContainer";
 import Paths from "../../../../routers/Paths.json";
 import { useEffect, useState } from "react";
+import EntComplaintCard from "../../../../components/ent/EntComplaintCard";
 
 const EntStudentComplaintsPage = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
 
     const [complaintsList, setComplaintsList] = useState([]);
 
     useEffect(() => {
-        EntStudentApi.getComplaintsData().then((response) => {
-            setComplaintsList(response.data);
-        });
+        setIsLoading(true);
+
+        EntStudentApi.getComplaintsData()
+            .then((response) => {
+                setComplaintsList(response.data.data);
+            })
+            .catch(() => {
+                // TODO: Add error handling
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+
     }, [])
 
     return (
         <EntPageContainer title="Reclamations">
+            {isLoading ? (
 
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+                <div className="text-center"><FontAwesomeIcon icon={faSpinner} className="text-lg loader" /></div>
 
-                <Link to={Paths.E_STUDENT_COMPLAINTS_CREATE_PAGE} className="p-5 flex justify-center items-center shadow rounded-lg border border-slate-100"><FontAwesomeIcon icon={faPlus} /></Link>
+            ) : (
 
-                <div className="hidden md:block xl:col-span-2"></div> {/* Grid Placeholder */}
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
 
-                {complaintsList.map(({ id, type, created_at, description }, i) =>
+                    <Link to={Paths.E_STUDENT_COMPLAINTS_CREATE_PAGE} className="p-5 flex justify-center items-center shadow rounded-lg border border-slate-100"><FontAwesomeIcon icon={faPlus} /></Link>
 
-                    <div key={i} className="relative p-5 shadow rounded-lg border border-slate-100">
+                    <div className="hidden md:block xl:col-span-2"></div> {/* Grid Placeholder */}
 
-                        <Link to={Paths.E_STUDENT_COMPLAINTS_DELETE_PAGE.replace(':id', id)} className="absolute top-0 right-0 translate-x-1/3 -translate-y-1/3 p-2 h-10 w-10 flex justify-center items-center rounded-full bg-slate-300"><FontAwesomeIcon icon={faTrashCan} className="text-white" /></Link>
+                    {complaintsList.map(({ id, type, created_at, description }, i) => (
 
-                        {/* <div className="text-slate-500 text-sm">{datetime.getDay()}/{datetime.getMonth()}/{datetime.getFullYear()}</div> */}
-                        <h1 className="font-semibold">#{id} | {type}</h1>
-                        <p>{description}</p>
+                        <EntComplaintCard key={id} id={id} type={type} created_at={created_at} description={description} />
 
-                    </div>
+                    ))}
 
-                )}
+                </div>
+            )}
 
-            </div>
 
         </EntPageContainer>
     );
