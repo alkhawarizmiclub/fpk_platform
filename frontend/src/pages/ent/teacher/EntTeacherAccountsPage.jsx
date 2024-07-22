a36d1390b37787f330fe8d40fa308d0a5f78025d
itsachrafmansari <contact@achrafmansari.com>
2024-07-22 10:24:17 +0100

    Modified the link button of each row on EntTeacherAccounts

--- frontend/src/pages/ent/teacher/EntTeacherAccountsPage.jsx
+++ frontend/src/pages/ent/teacher/EntTeacherAccountsPage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import EntTeacherApi from "../../../api/EntTeacherApi";
 import EntPageContainer from "../../../components/ent/EntPageContainer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner, faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const EntTeacherAccountsPage = () => {
 
@@ -54,7 +54,10 @@ const EntTeacherAccountsPage = () => {
                                     <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-800 text-center">{account_id}</td>
                                     <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-800 text-center">{account_password}</td>
                                     <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-blue-600 underline text-center">
-                                        <a href={account_url} target="_blank" rel="noopener noreferrer" className="hover:text-blue-800">Lien</a>
+                                        <a href={account_url} target="_blank" rel="noopener noreferrer" className="px-4 py-2 space-x-2 rounded-lg text-sm text-white bg-orange-400 hover:bg-orange-300 transtition-colors duration-default">
+                                            <FontAwesomeIcon icon={faUpRightFromSquare} />
+                                            <span>Lien</span>
+                                        </a>
                                     </td>
                                 </tr>
                             )) : (
